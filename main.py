d9a4f381a4d9b465df1179c33b54457c19f3aaf0
gilBeckerManna <gil.becker@manna-irrigation.com>
2020-11-29 08:17:37 +0200

    image_histogram step2

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import numpy as np
 import requests
 import skimage.io
 import skimage.viewer
+from matplotlib import pyplot as plt
+import pandas
 
 URL = 'https://www.researchgate.net/profile/Tao_Chen15/publication/3935609/figure/fig1/AS:394647298953219@1471102656485/8-bit-256-x-256-Grayscale-Lena-Image.png'
 
@@ -16,15 +18,20 @@ with open(image_file_name, 'wb') as image_file:
     image_file.write(image_data)
 
 # read the file from local directory
-image = skimage.io.imread(fname=os.path.join(os.getcwd(), image_file_name), as_gray=True)
+image = skimage.io.imread(fname=os.path.join(os.getcwd(), image_file_name), as_gray=False)
 
 # create the histogram
-pixels_population, tonal = np.histogram(image, bins=256, range=(0, 1))
+pixels_population, tonal = np.histogram(image, bins=256)
 
 # Save the tonal values into a CSV
-np.savetxt("histogram.csv", tonal, fmt='%10.5f', delimiter=",")
+data_frame = pandas.DataFrame({'tonal': tonal[:-1], 'pixel_population': pixels_population})
+data_frame.set_index('tonal', inplace=True)
+data_frame.to_csv(os.path.join(os.getcwd(), 'histogram.csv'))
 
 # Sort the values of the tonal highest to low
 tonal[::-1].sort()
+
+
+
 
 
